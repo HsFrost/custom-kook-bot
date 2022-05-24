5d9e7e5526e947252c90a5f0f87f3b013c7ad6db
HsFrost <2384232170@qq.com>
2022-05-24 21:14:43 +0800

    1/2

--- khl-bot/main.py
+++ khl-bot/main.py
@@ -1,21 +1,22 @@
 from khl import Bot, Message
-import random
 import json
 import logging
 import time
-#控制台日志输出
+import random
+import requests
+
+# 控制台日志输出
 logging.basicConfig(level='INFO')
 
+# 导入凭证json
+with open('config/khl-bot-config.json', 'r', encoding='utf-8') as a, open('config/apexlegends-api.json', 'r', encoding='utf-8') as b:
+    khl_bot_config = json.load(a)
+    apexlegends_api = json.load(b)
 
-#导入凭证json
-with open('config\config.json', 'r', encoding='utf-8') as f:
-    config = json.load(f)
+# bot凭证
+bot = Bot(token=khl_bot_config['token'])
 
-#bot凭证
-bot = Bot(token=config['token'])
-
-
-
+# 指令
 @bot.command(name='hello')
 async def roll(msg: Message,str:str=''):
     if str == 'time':
@@ -25,7 +26,19 @@ async def roll(msg: Message,str:str=''):
     elif str == 'help':
         await msg.reply('什么咱们这机器人哪有那种高级功能')
     else:
-        await msg.reply('Error')
+        await msg.reply('Error,some thing has wrong')
+
+@bot.command(name='apex')
+async def roll(msg: Message,player:str='',platform:str='PC'):
+    auth=apexlegends_api['auth_key']
+    if player != '':
+        solve = requests.get(f"https://api.mozambiquehe.re/bridge?auth={auth}&player={player}&platform={platform}")
+
+    elif player == '':
+        await msg.reply("Error,can't solve,maby you did not input PlayerNAME.")
+
+#GET https://api.mozambiquehe.re/bridge?auth=YOUR_API_KEY&player=PLAYER_NAME&platform=PLATFORM
+
 @bot.command(name='shaizi')
 async def roll(msg: Message, t_min: int, t_max: int, n: int = 1):
     result = [random.randint(t_min, t_max) for i in range(n)]
