a9db44f4904ab8509fba82e4f4d91f731a5d2b52
HsFrost <2384232170@qq.com>
2022-05-25 18:30:59 +0800

    debug√

--- khl-bot/main.py
+++ khl-bot/main.py
@@ -4,8 +4,8 @@ import time
 import random
 import requests
 
-from khl import Bot, Message, EventTypes, Event
-from khl.card import CardMessage, Card, Module, Element, Types, Struct
+from khl import Bot, Message
+from khl.card import CardMessage, Card, Module, Element, Types
 
 # 控制台日志输出
 logging.basicConfig(level='INFO')
@@ -24,17 +24,17 @@ auth = apex_legends_api['auth_key']
 
 # hello指令
 @bot.command(name='hello')
-async def hello(msg: Message, str: str = ''):
-    '''
-    :param msg: (忽略)
-    :param str: 指令['','time','help']
-    :return:
-    '''
-    if str == '':
+async def hello(msg: Message, term: str = ''):
+
+    # :param msg: (忽略)
+    # :param term: 指令['','time','help']
+    # :return:
+
+    if term == '':
         await msg.reply('world for you!')
-    elif str == 'time':
+    elif term == 'time':
         await msg.reply(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-    elif str == 'help':
+    elif term == 'help':
         await msg.reply('什么咱们这机器人哪有那种高级功能')
     else:
         await msg.reply('Error,some thing has wrong')
@@ -42,24 +42,26 @@ async def hello(msg: Message, str: str = ''):
 
 # apex战绩查询
 @bot.command(name='apex')
-async def apex(msg: Message, str: str = '', player: str = '', platform: str = 'PC'):
-    '''
-    :param msg: (默认值)
-    :param str: 指令
-    :param player: 玩家ID
-    :param platform: 玩家平台 ['PC'(Origin or Steam),'PS4'(Playstation 4/5) or 'X1'(Xbox)]
-    :return:
-    '''
-    if str == '查询':
+async def apex(msg: Message, term: str = '', player: str = '', platform: str = 'PC'):
+
+    # :param msg: (默认值)
+    # :param term: 指令
+    # :param player: 玩家ID
+    # :param platform: 玩家平台 ['PC'(Origin or Steam),'PS4'(Playstation 4/5) or 'X1'(Xbox)]
+    # :return:
+
+    if term == '查询':
         # solve为向api发送请求的返回解
+        # GET https://api.mozambiquehe.re/bridge?auth=YOUR_API_KEY&player=PLAYER_NAME&platform=PLATFORM
         solve = (
             requests.get(f"https://api.mozambiquehe.re/bridge?auth={auth}&player={player}&platform={platform}")).json()
         if "Error" not in solve.keys():
             # 几组玩家参数
             player_name = f'玩家：{solve["global"]["name"]}'
             player_level = f'等级：{solve["global"]["level"]}'
-            player_rank = f'段位：{solve["global"]["rank"]["rankName"]}{solve["global"]["rank"]["rankDiv"]}:{solve["global"]["rank"]["rankScore"]}'
-
+            player_rank = f'段位：{solve["global"]["rank"]["rankName"]}{solve["global"]["rank"]["rankDiv"]}:'\
+                          f'{solve["global"]["rank"]["rankScore"]} '
+            #回复
             await msg.reply(
                 CardMessage(Card(
                     # 子项
@@ -71,9 +73,13 @@ async def apex(msg: Message, str: str = '', player: str = '', platform: str = 'P
                                    accessory=Element.Image(src=f'{solve["global"]["rank"]["rankImg"]}'),
                                    mode=Types.SectionMode.RIGHT),
                     # 子项
+                    Module.Divider(),
+                    # 子项
+                    Module.Section(f'常用角色:{solve["legends"]["selected"]["LegendName"]}'),
+                    # 子项
                     Module.Container(Element.Image(src=f'{solve["legends"]["selected"]["ImgAssets"]["icon"]}')),
                     # 颜色设置
-                    color='#F8FF18'))
+                    color='#DE1717'))
             )
         # 选择支
         elif "Error" in solve.keys():
@@ -82,29 +88,26 @@ async def apex(msg: Message, str: str = '', player: str = '', platform: str = 'P
         else:
             await msg.reply("Error,some thing has wrong")
     # 选择支
-    elif str == '地图':
+    elif term == '地图':
         await msg.reply("暂时未实装")
     # 选择支
-    elif str == '帮助':
+    elif term == '帮助':
         await msg.reply("暂时未实装")
     # 选择支
     else:
         await msg.reply("Command not Found")
 
 
-# GET https://api.mozambiquehe.re/bridge?auth=YOUR_API_KEY&player=PLAYER_NAME&platform=PLATFORM
-
-
 # 掷色子
 @bot.command(name='roll')
 async def roll(msg: Message, t_min: int, t_max: int, n: int = 1):
-    '''
-    :param msg: （忽略）
-    :param t_min: 色子的最小点数
-    :param t_max: 色子的最大点数
-    :param n:     色子掷几次
-    :return:
-    '''
+
+    # :param msg: （忽略）
+    # :param t_min: 色子的最小点数
+    # :param t_max: 色子的最大点数
+    # :param n:     色子掷几次
+    # :return:
+
     result = [random.randint(t_min, t_max) for i in range(n)]
     await msg.reply(f'you got: {result}')
 
